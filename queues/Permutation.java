e1cd358a2697bccb9435feda0d5af05b877ea197
geoforce <glenn.ceurvels@gmail.com>
2018-03-02 16:37:57 +0100

    Permutation + shell script

--- queues/Permutation.java
+++ queues/Permutation.java
@@ -1,3 +1,20 @@
-public class Permutation {
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdRandom;
 
+public class Permutation {
+    public static void main(String[] args) {
+        if (StdIn.isEmpty()) {
+            throw new IllegalArgumentException("Text input provided");
+        }
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Length argument required");
+        }
+
+        String[] val = StdIn.readAllStrings();
+        int k = Integer.valueOf(args[0]);
+
+        for (int i = 0; i < k; i++) {
+            System.out.println(val[StdRandom.uniform(k+1)]);
+        }
+    }
 }
---
+++ queues/permutation.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# It's currently a bit tedious to read from StdIn from an IntelliJ run configuration
+javac -cp ../lib/algs4.jar:. -d ../target *.java;
+cd ../target && java -cp ../lib/algs4.jar:. Permutation "$@";
