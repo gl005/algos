d4e3249516b957787ed144dd0a7aaea5403f8234
geoforce <glenn.ceurvels@gmail.com>
2018-03-08 11:41:31 +0100

    fast solution

--- collinear-testing/FastCollinearPoints.java
+++ collinear-testing/FastCollinearPoints.java
@@ -46,7 +46,10 @@ public class FastCollinearPoints {
             return lineSegments;
         }
 
-        for (Point point : points) {
+        Point[] copy = Arrays.copyOf(points, numPoints);
+
+        for (Point point : copy) {
+            System.out.println(point.toString());
             Arrays.sort(points, point.slopeOrder());
             pushSegment(checkColinear());
         }
@@ -100,24 +103,24 @@ public class FastCollinearPoints {
     }
 
     public static void main(String[] args) {
-        Point p1 = new Point(3,3);
+        Point p1 = new Point(3,3); // a
         Point p2 = new Point(5,7);
-        Point p3 = new Point(4,4);
-        Point p4 = new Point(1,1);
+        Point p3 = new Point(4,4); // a
+        Point p4 = new Point(1,1); // a
         Point p5 = new Point(5,1);
-        Point p6 = new Point(6,6);
+        Point p6 = new Point(6,6); // a|b
         Point p7 = new Point(7,50);
         Point p8 = new Point(3,8);
-        Point p9 = new Point(6, 3);
-        Point p10 = new Point(6, 1);
-        Point p11 = new Point(6, 2);
-        Point p12 = new Point(0, 0);
-        Point p13 = new Point(15, 15);
+        Point p9 = new Point(6, 3); // b
+        Point p10 = new Point(6, 1); // b
+        Point p11 = new Point(6, 2); // b
+        Point p12 = new Point(0, 0); // a
+        Point p13 = new Point(15, 15); // a
 
 
-        Point[] allPoints = new Point[]{p1,p2,p3,p4,p5,p6,p7,p8,p9,p11,p10, p12, p13};
+        Point[] allPoints = new Point[]{p1,p2,p3,p5,p6,p7,p8,p9,p11,p10, p12, p13};
 
-        BruteCollinearPoints pfcp = new BruteCollinearPoints(allPoints);
+        FastCollinearPoints pfcp = new FastCollinearPoints(allPoints);
         LineSegment[] segments = pfcp.segments();
         int i = 0;
     }
