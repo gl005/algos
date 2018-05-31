1133fc785fae1b3503d68dcf2c34d59a943bd4d1
geoforce <glenn.ceurvels@gmail.com>
2018-05-31 17:24:11 +0200

    board + solver 1

--- puzzle/Board.java
+++ puzzle/Board.java
@@ -1,16 +1,19 @@
-import java.util.Arrays;
-import java.util.Collections;
+import edu.princeton.cs.algs4.MinPQ;
+
+import java.util.*;
 
 public class Board {
 
     protected final int[][] blocks;
     private int dimension;
+    private int blockCount;
 
     // construct a board from an n-by-n array of blocks
     // (where blocks[i][j] = block in row i, column j)
     public Board(int[][] blocks)  {
         this.blocks = deepCopy(blocks);
         dimension = this.blocks.length;
+        blockCount = dimension * dimension;
     }
 
     // board dimension n
@@ -20,37 +23,56 @@ public class Board {
 
     // number of blocks out of place
     public int hamming() {
-        return 0;
+        return blocksOutOfPlace(false).size();
     }
 
     // sum of Manhattan distances between blocks and goal
     public int manhattan() {
-        return 0;
+        int sum = 0;
+        for (Integer distance: blocksOutOfPlace(false)) {
+            sum += distance;
+        }
+        return sum;
     }
 
     // is this board the goal board?
     public boolean isGoal() {
-        int i = 1;
-        int lastIndex = (dimension*dimension)-1;
-        for (int[] row : blocks) {
-            for (int cell : row) {
-                if (i == lastIndex) {
-                    i = 0;
-                }
-                else if (i != cell) {
-                    return false;
-                }
-                else {
-                    i++;
-                }
-            }
-        }
-        return true;
+        return blocksOutOfPlace(true).isEmpty();
     }
 
     // a board that is obtained by exchanging any pair of blocks
     public Board twin() {
         return new Board(blocks);
+    }
+
+    // all neighboring boards
+    public Iterable<Board> neighbors() {
+        List<Board> neighbours = new ArrayList<>();
+        for (int i = 0; i < dimension; i++) {
+            for (int j = 0; j < dimension; j++) {
+                int value = blocks[i][j];
+                if (value != 0) continue;
+
+                //check upper neighbour
+                if (i > 0) {
+                    neighbours.add(swap(i, j, i-1, j));
+                }
+                //check right neighbour
+                if (j < dimension-1) {
+                    neighbours.add(swap(i, j, i, j+1));
+                }
+                //check lower neighbour
+                if (i < dimension-1) {
+                    neighbours.add(swap(i, j,  i+1, j));
+                }
+                //check left neighbour
+                if (j > 0) {
+                    neighbours.add(swap(i, j, i, j - 1));
+                }
+                return neighbours;
+            }
+        }
+        return null;
     }
 
     // does this board equal y?
@@ -61,23 +83,49 @@ public class Board {
         return this == y || Arrays.deepEquals(blocks, ((Board)y).blocks);
     }
 
-    // all neighboring boards
-    public Iterable<Board> neighbors() {
-        return Collections.emptyList();
-    }
-
     // string representation of this board (in the output format specified below)
     public String toString() {
-        return "";
+        StringBuilder sb = new StringBuilder();
+        for (int[] row:blocks) {
+            for (int value:row) {
+                sb.append((value == 0 ? "-": value) +"\t");
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
     }
 
-    // unit tests (not graded)
-    public static void main(String[] args) {
-        Board a = new Board(new int[][]{{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,0}});
-        Board b = new Board(new int[][]{{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,0}});
 
-        System.out.println(a.equals(b));
-        System.out.println(a.isGoal());
+    private List<Integer> blocksOutOfPlace(boolean breakOnFirst) {
+        int place = 1;
+        List<Integer> distanceOutOfPlace = new ArrayList<>();
+
+        for (int[] row : blocks) {
+            for (int value : row) {
+                int stepsToValue = Math.abs(value == 0 ? (blockCount - 1 - place): value - place);
+
+                if ((value == 0 && place != blockCount -1) || place != value) {
+                    distanceOutOfPlace.add(stepsToValue);
+                }
+
+                if (breakOnFirst && !distanceOutOfPlace.isEmpty()) {
+                    return distanceOutOfPlace;
+                }
+                place++;
+            }
+        }
+        return distanceOutOfPlace;
+    }
+
+    private Board swap(int fromRow, int fromCell, int toRow, int toCell) {
+        int[][] copy = deepCopy(blocks);
+
+        int fromValue = copy[fromRow][fromCell];
+        int toValue = copy[toRow][toCell];
+        copy[toRow][toCell] = fromValue;
+        copy[fromRow][fromCell] = toValue;
+
+        return new Board(copy);
     }
 
     private int[][] deepCopy(int[][] original) {
@@ -90,5 +138,28 @@ public class Board {
             result[i] = Arrays.copyOf(original[i], original[i].length);
         }
         return result;
+    }
+
+    // unit tests (not graded)
+    public static void main(String[] args) {
+        Board a = new Board(new int[][]{{0,1,3},{4,2,5},{7,8,6}});
+        Board b = new Board(new int[][]{{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,0}});
+
+        System.out.print(a.toString());
+        System.out.println("steps to goal: "+a.manhattan());
+        System.out.println("blocks out of place: "+a.hamming());
+        System.out.println("is goal: "+a.isGoal());
+        System.out.println(" ");
+
+//        for (Board board : a.neighbors()) {
+//            System.out.print(board.toString());
+//            //System.out.println("is goal: "+board.isGoal());
+//            System.out.println("steps to goal: "+board.manhattan());
+//            System.out.println("blocks out of place: "+board.hamming());
+//            System.out.println(" ");
+//        }
+//
+//        System.out.println(a.equals(b));
+//        System.out.println(a.isGoal());
     }
 }
--- puzzle/Solver.java
+++ puzzle/Solver.java
@@ -1,31 +1,86 @@
-import java.util.Collections;
+import edu.princeton.cs.algs4.MinPQ;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 public class Solver {
 
     private final Board initialBoard;
 
+    private boolean isSolvable;
+
+    private int numberOfSteps;
+
+    private List<Board> path;
+
     // find a solution to the initial board (using the A* algorithm)
     public Solver(Board initial) {
         this.initialBoard = initial;
+        Comparator<Board> hammingComparator = (boardA, boardB) -> {
+            int priorityA = boardA.hamming() + boardA.manhattan();
+            int priorityB = boardB.hamming() + boardA.manhattan();
+            if (priorityA > priorityB) {
+                return 1;
+            }
+            if (priorityA == priorityB) {
+                return 0;
+            } else {
+                return -1;
+            }
+        };
+        MinPQ<Board> frontier = new MinPQ<Board>(initialBoard.dimension(), hammingComparator);
+        path = new ArrayList<>();
+
+        frontier.insert(initialBoard);
+        while (!frontier.isEmpty()) {
+            Board current = frontier.delMin();
+            int currentHamming = current.hamming();
+            boolean solvable = false;
+            path.add(current);
+
+            if (current.isGoal()) {
+                break;
+            }
+
+            for (Board neighbour : current.neighbors()) {
+                frontier.insert(neighbour);
+                if (neighbour.hamming() < currentHamming) {
+                    solvable = true;
+                }
+            }
+
+            if (solvable == false) {
+                isSolvable = solvable;
+                break;
+            }
+        }
+        numberOfSteps = isSolvable ? path.size() -1: -1;
     }
 
     // is the initial board solvable?
     public boolean isSolvable() {
-        return false;
+        return isSolvable;
     }
 
     // min number of moves to solve initial board; -1 if unsolvable
     public int moves() {
-        return 10;
+        return numberOfSteps;
     }
 
     // sequence of boards in a shortest solution; null if unsolvable
     public Iterable<Board> solution() {
-        return Collections.emptyList();
+        return path;
     }
 
     // solve a slider puzzle (given below)
     public static void main(String[] args) {
+        Board a = new Board(new int[][]{{0,1,3},{4,2,5},{7,8,6}});
+        Solver solver = new Solver(a);
 
+        for (Board board : solver.solution()) {
+            System.out.println(board);
+            System.out.println(" ");
+        }
     }
 }
